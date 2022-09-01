5c136c68d3060acb45b8f0e957d4ae8374675322
Hector Gonzalez <gorozco.hector@gmail.com>
2022-09-01 11:19:28 -0400

    Make popup to create todos

--- src/main.js
+++ src/main.js
@@ -2,4 +2,51 @@ import './style.css';
 import './webpage.js'
 
 
-console.log('sirvesi');
+//todo factory function
+const Todo = function(title, description, dueDate, priority, notes, list) {
+
+    return {title, description, dueDate, priority, notes, list}
+}
+
+const List = function(name){
+    let content = {};
+    //start with empty content if not provided
+
+    const length = function (){
+        return Object.keys(content).length;
+    };
+
+    return {name, content, lenght}
+}
+
+const todoManager = (
+    function () {
+        let todos = {};
+
+        function addTodo(data){
+
+        }
+
+        PubSub.subscribe('pressed-add-button', addTodo)
+    }
+)();
+
+const listManager = (
+    function () {
+        let lists = {};
+
+        function addList(data){
+
+        }
+
+        const getLists = function(){
+            return lists
+        }
+
+        PubSub.subscribe('pressed-add-list', addList)
+    }
+)();
+
+
+
+const newTodo = Todo('juan', 'as;dflajsd;fl', 12, 'top', 'nada', 'default');
--- src/webpage.js
+++ src/webpage.js
@@ -1,10 +1,68 @@
+import PubSub from "pubsub-js";
+
 const webpage = (
     function () {
+        
+
+        
+    }
+)();
+
+const header = (
+    function () {
+        const _header = document.querySelector('header');
         const _buttonOpenMenu = document.querySelector('#button-open-menu');
-        const _sideBar = document.querySelector('#sidebar');
+        const _buttonAdd = document.querySelector('#button-add-todo');
 
         _buttonOpenMenu.addEventListener('click', ()=> {
-            _sideBar.classList.toggle('hidden')
-        })
+            console.log('pressed')
+            PubSub.publish('pressed-menu-button')
+        });
+
+
+        _buttonAdd.addEventListener('click', () => {
+            PubSub.publish('pressed-add-button')
+        });
+    }        
+)();
+
+const sidebar = (
+    function () {
+        const _sideBar = document.querySelector('#sidebar');
+        const _listsArea = document.querySelector('#lists-area')
+        
+        //hide or show sidebar
+        const _toggle = function () {
+            _sideBar.classList.toggle('hidden');
+        };
+
+        PubSub.subscribe('pressed-menu-button', _toggle);
+    }
+)();
+
+const popup = (
+    function() {
+        const _popup = document.querySelector('#popup');
+        const _listsSelector = document.querySelector('#add-todo-list');
+        const _buttonCreate = document.querySelector('#popup-button');
+        
+        //hide or show popup
+        const _toggle= function (){
+            _popup.classList.toggle('invisible')
+        }
+
+        const _populateListsSelector = function () {
+
+        }
+
+        //this function is ran when pressing the "create" button
+        const _createTodo = function (button) {
+            button.preventDefault();
+            _toggle();
+        }
+
+        _buttonCreate.addEventListener('click', _createTodo)
+
+        PubSub.subscribe('pressed-add-button', _toggle)
     }
 )();
